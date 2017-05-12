1fd4829881949ccaa51760f81a8c6d41ea039fb8
Andres A. Cevallos Prado <andres.cevallos@webfg.com>
2017-05-12 16:30:18 +0200

    better reports code coverage and server, stack trace WIP

--- src/Template/HtmlTemplate.php
+++ src/Template/HtmlTemplate.php
@@ -13,6 +13,7 @@ use ChillDebug\Helper\Filesystem;
 class HtmlTemplate extends Abstracted
 {
     const PROTOCOL_URL = 'phpstorm://open?url=file://%s&line=%d';
+
     /**
      * @param array $stackTrace
      *
@@ -22,13 +23,13 @@ class HtmlTemplate extends Abstracted
     {
         $template = file_get_contents(__DIR__ . '/../Resources/templates/html_report.html');
 
-        $coverage = $this->codeCoverageMapper(json_decode(file_get_contents($fileName.'.cvg'), true));
-        $server = file_get_contents($fileName.'.svr');
-        $trace = file_get_contents($fileName.'.xt');
+        $coverage = $this->codeCoverageMapper(json_decode(file_get_contents($fileName . '.cvg'), true));
+        $server   = $this->serverMapper(json_decode(file_get_contents($fileName . '.svr'), true));
+        $trace    = file_get_contents($fileName . '.xt');
 
         $report = '';
 
-        Filesystem::dump($fileName . '.html', sprintf($template, $fileName, '', $coverage, ''));
+        Filesystem::dump($fileName . '.html', sprintf($template, $fileName, '', $coverage, $server));
     }
 
     /**
@@ -58,7 +59,8 @@ class HtmlTemplate extends Abstracted
             unset ($fileInformation['lines_coverage']);
             foreach ($fileInformation as $lineInformation) {
                 foreach ($lineInformation as $lineNumber => $singleLineInformation) {
-                    $report .= '<tr><td><a href="'. sprintf(self::PROTOCOL_URL, $file, $lineNumber) .'">'.$file.':'.$lineNumber.'</a></td>';
+                    $report .= '<tr><td><a href="' . sprintf(self::PROTOCOL_URL, $file,
+                            $lineNumber) . '">' . $file . ':' . $lineNumber . '</a></td>';
                     foreach ($singleLineInformation as $infoKey => $infoContent) {
                         if ($infoKey == 'content') {
                             $report .= '<td><pre class="prettyprint">' . $infoContent . '</pre></td>';
@@ -79,37 +81,21 @@ class HtmlTemplate extends Abstracted
     {
         $header = '<div class="alert alert-info">';
         $header .= '<ul class="list-group">';
-        $header .= '<li class="list-group-item">Total time: ' . $informationAsArray['total_time_execution'] . '</li>';
-        $header .= '<li class="list-group-item">Peak memory usage: ' . $informationAsArray['peak_memory_usage'] . '</li>';
+        $header .= '<li class="list-group-item">Time: ' . $informationAsArray['time'] . '</li>';
         $header .= '</ul>';
         $header .= '</div>';
-
+        unset($informationAsArray['time']);
         $report = $header;
 
         $this->removeCommonInfo($informationAsArray);
 
-        $report .= '<table class="table">';
+        $report .= '<ul class="table">';
 
-        foreach ($informationAsArray as $file => $fileInformation) {
-            $report .= '<tr><td><strong>File:</strong></td><td colspan="2">' . $file . '</td></tr>';
-            $report .= '<tr><td><strong>File coverage:</strong></td><td colspan="2">' . $fileInformation['lines_coverage'] . ' % </td></tr>';
-            $report .= '<tr><td><strong>File </strong></td><td><strong>Executions</strong></td><td><strong>Code</strong></td></tr>';
-            unset ($fileInformation['lines_coverage']);
-            foreach ($fileInformation as $lineInformation) {
-                foreach ($lineInformation as $lineNumber => $singleLineInformation) {
-                    $report .= '<tr><td><a href="'. sprintf(self::PROTOCOL_URL, $file, $lineNumber) .'">'.$file.':'.$lineNumber.'</a></td>';
-                    foreach ($singleLineInformation as $infoKey => $infoContent) {
-                        if ($infoKey == 'content') {
-                            $report .= '<td><pre class="prettyprint">' . $infoContent . '</pre></td>';
-                        } else {
-                            $report .= '<td>' . $infoContent . '</td>';
-                        }
-                    }
-                    $report .= '</tr>';
-                }
-            }
+        foreach ($informationAsArray as $infoKey => $information) {
+            $report .= '<li class="list-group-item">' . ucfirst($infoKey) . '<pre class="prettyprint">' . json_encode($information,
+                JSON_PRETTY_PRINT) . '</pre></li>';
         }
-        $report .= '</table>';
+        $report .= '</ul>';
 
         return $report;
     }
